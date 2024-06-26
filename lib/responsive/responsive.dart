46b67fc8514e0ef2b654ad0cb300e1d0703f5ff6
Rogerio <you@example.com>
2024-06-26 17:57:45 -0300

    Atualização MVP1

---
+++ lib/responsive/responsive.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+
+class ResponsiveLayout extends StatelessWidget {
+  final Widget iphone;
+  final Widget ipad;
+  final Widget macbook;
+  final bool? forceIpadList;
+  const ResponsiveLayout(
+      {Key? key,
+      required this.ipad,
+      required this.iphone,
+      required this.macbook,
+      this.forceIpadList = false})
+      : super(key: key);
+
+  static int iphoneLimit = 600;
+  static int ipadVerticalLimit = 800;
+  static int ipadLimit = 1200;
+
+  static bool isIphone(BuildContext context) =>
+      MediaQuery.of(context).size.width < iphoneLimit;
+
+  static bool isIpad(BuildContext context) =>
+      MediaQuery.of(context).size.width < ipadLimit &&
+      MediaQuery.of(context).size.width >= iphoneLimit;
+
+  static bool isMacbook(BuildContext context) =>
+      MediaQuery.of(context).size.width >= ipadLimit;
+
+  @override
+  Widget build(BuildContext context) {
+    return LayoutBuilder(
+        builder: (BuildContext context, BoxConstraints constraints) {
+      if (constraints.maxWidth < iphoneLimit) {
+        return iphone;
+      }
+      if (constraints.maxWidth > iphoneLimit &&
+          constraints.maxWidth < ipadVerticalLimit) {
+        if(forceIpadList != null && forceIpadList == true){
+          return ipad;
+        }else{
+          return iphone;
+        }
+      }
+
+      if (constraints.maxWidth < ipadLimit) {
+        return ipad;
+      } else {
+        return macbook;
+      }
+    });
+  }
+}
---
+++ lib/responsive/responsive_framework.dart
@@ -0,0 +1,7 @@
+library responsive_framework;
+
+export './responsive_grid.dart';
+export './responsive_row_colunm.dart';
+export './responsive_value.dart';
+export './responsive_wrapper.dart';
+export './scroll_behavior.dart';
